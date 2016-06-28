9f5c0c36d0ae6e970c76fd2739fcb8df48bbe2d3
Zohaib <zohaibahmedbutt@gmail.com>
2016-06-28 17:03:42 +0500

    Movie: Implemented CKEditor and Trailers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+  validates :description, length: { maximum: 255 }
+  validates :trailer_url, length: { maximum: 1000 }
 end
