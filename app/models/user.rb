58595a3e14b01f54104f0f6c1769b64bcb551416
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-02 15:05:02 +0500

    Movie: Improved New and Edit Movie Forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ActiveRecord::Base
 
   GENDERS =  ['Male', 'Female']
 
+  validates :first_name, :last_name, :birth_date, presence: true
+  validates :gender, inclusion: { in: %w(Male Female), message: "%{value} is not valid" }
+
   def full_name
     [self.first_name , self.last_name].join(" ")
   end
