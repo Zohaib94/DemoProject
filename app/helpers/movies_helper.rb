f2b3ae96fd6f7f9ddf55ec968816f90c0cdb6d38
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-18 15:07:11 +0500

    Movie: Functionality to avoid script execution in trailer embed url

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
 module MoviesHelper
+
+    def escape_script_tags(embed_url)
+      if embed_url.include?('<script>') && embed_url.include?('</script>')
+        safe_url = html_escape(embed_url)
+      else
+        embed_url
+      end
+    end
+
 end
