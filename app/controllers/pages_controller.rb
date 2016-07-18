36e63b98e29595c2a4a59581a3790689209ef49a
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-18 18:25:00 +0500

    Homepage: Added Default text and implemented recommendations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,8 @@ class PagesController < ApplicationController
   end
 
   def home
-    @featured = Movie.featured.approved.first(4)
-    @latest = Movie.latest.approved.first(4)
-    @top = Movie.top.approved.first(4)
+    @featured_movies = Movie.featured.approved.first(4)
+    @latest_movies = Movie.latest.approved.first(4)
+    @top_movies = Movie.top.approved.first(4)
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,8 @@ module PagesHelper
   def get_attachment_object(resource)
     resource.attachment || Attachment.new
   end
+
+  def sections_exist?(first_section, second_section, third_section)
+    first_section || second_section || third_section
+  end
 end
