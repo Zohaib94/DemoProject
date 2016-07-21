f4fcce844ee936505b7a9627231e01bdd852b1c4
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-21 11:11:22 +0500

    Controller: Implemented exception handling for ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :gender, :birth_date])
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :gender, :birth_date, attachment_attributes: [:id, :image, :_destroy]])
+    end
+
+    def not_found
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
     end
 end
