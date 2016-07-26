90f824109d048c61ffba0e3e5f96576ee259c379
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-26 17:41:09 +0500

    Pages: Controller renamed to Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
-class PagesController < ApplicationController
-
+class HomeController < ApplicationController
   def index
-  end
-
-  def home
     @featured_movies = Movie.featured.approved.first(4)
     @latest_movies = Movie.latest.approved.first(4)
     @top_movies = Movie.top.approved.first(4)
