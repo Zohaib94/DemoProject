59782d391676ec5e7b80c124c947cd87bf51041e
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-26 12:05:18 +0500

    Movie: Refactored Model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
 
   DEFAULT_SEARCH_ORDER = 'updated_at DESC'
+  GENRES = %w(Action Horror Comedy Thriller Romance Sci-Fi Sports Tragedy Animated)
+  TYPES = %w(latest featured)
 
   paginates_per 12
 
@@ -19,10 +21,6 @@ class Movie < ActiveRecord::Base
 
   accepts_nested_attributes_for :attachments, allow_destroy: true , reject_if: proc { |attributes| attributes['image'].blank? }
 
-  GENRES = %w(Action Horror Comedy Thriller Romance Sci-Fi Sports Tragedy Animated)
-
-  TYPES = %w(latest featured)
-
   scope :latest, -> { order(release_date: :desc) }
   scope :approved, -> { where(approved: true) }
   scope :featured, -> { where(featured: true).order(updated_at: :desc) }
@@ -31,7 +29,8 @@ class Movie < ActiveRecord::Base
 
 
   def image_path
-    attachments.first ? attachments.first.image.url(:medium) : 'medium/missing.png'
+    first_attachment = attachments.first
+    first_attachment ? first_attachment.image.url(:medium) : 'medium/missing.png'
   end
 
   def self.get_movies(type_param)
