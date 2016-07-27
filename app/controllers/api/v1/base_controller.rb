dffe84994b5b2a455fab9847187ac869aa9883ac
Zohaib <zohaibahmedbutt@gmail.com>
2016-07-27 12:50:35 +0500

    Movie: Added API Authentication

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::BaseController < ApplicationController
+  before_action :authenticate_request
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  TOKEN = 'C167417CA74E3222'
+
   def not_found
-    render json: {error: "record not found", status: 404}, status: 404
+    render json: { error: "record not found", status: 404 }
   end
+
+  private
+    def authenticate_request
+      head :unauthorized unless request.headers['Authorization'] == TOKEN
+    end
 end
